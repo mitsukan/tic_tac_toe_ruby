443cbd1f0202ed693effebd37d9bfa8f389dd878
Jihin Ip <jim_ip20@hotmail.com>
2018-07-05 15:16:42 +0100

    Test for player O movement in Players class

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -4,15 +4,23 @@ describe Players do
 
   mock_grid = [["","",""],["","",""],["","",""]]
 
-  describe ".X_moves" do
+  describe ".move_X" do
 
-    it "Can add a move to a grid" do
+    it "X add a move to a grid" do
       subject.move_X(mock_grid,1,1);
       expect(mock_grid).to eq([["","",""],["","X",""],["","",""]])
     end
 
   end
 
+  describe ".move_O" do
+
+    it "O can add a move to a grid" do
+      subject.move_O(mock_grid,1,1);
+      expect(mock_grid).to eq([["","",""],["","O",""],["","",""]])
+    end
+
+  end
 
 
 end
